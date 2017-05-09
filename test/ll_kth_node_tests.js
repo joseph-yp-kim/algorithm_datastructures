462f6e37b83eb47bd1bd3a54cf3e597e7d705635
Joseph Kim <canyouspelljoe@gmail.com>
2017-05-09 11:54:55 -0700

    sum lists

--- test/ll_kth_node_tests.js
+++ test/ll_kth_node_tests.js
@@ -1,28 +1,19 @@
 const expect = require('chai').expect;
-const LinkedList = require('./../src/ll_remove_dupes').ll;
-const Node = require('./../src/ll_remove_dupes').nd;
+const LinkedList = require('./../src/ll_kth_node').ll;
+const Node = require('./../src/ll_kth_node').nd;
 
-describe('Remove Duplicates from Linked List', () => {
+describe('Return kth to last', () => {
 
-  it('should remove duplicates', () => {
-    const list1 = new LinkedList(1, 7, 2, 3, 2);
-    const list2 = new LinkedList(4, 2, 1, 2, 6, 8, 9, 6);
-    const list3 = new LinkedList(5, 4, 4, 6, 8);
-    list1.rmvDupes();
-    list2.rmvDupes();
-    list3.rmvDupes();
-    expect(list1).to.eql(new LinkedList(1, 7, 3));
-    expect(list2).to.eql(new LinkedList(4, 1, 8, 9));
-    expect(list3).to.eql(new LinkedList(5, 6, 8));
-  });
-
-  it('should return origin ll if no duplicates', () => {
-    const list1 = new LinkedList(5, 1, 2, 6, 8);
-    const list2 = new LinkedList(3, 9, 1, 2);
-    list1.rmvDupes();
-    list2.rmvDupes();
-    expect(list1).to.eql(new LinkedList(5, 1, 2, 6, 8));
-    expect(list2).to.eql(new LinkedList(3, 9, 1, 2));
+  it('should return kth to last element in singly last node', () => {
+    const list1 = new LinkedList(1, 2, 3, 4, 5);
+    const list2 = new LinkedList(1, 2, 1, 2, 6, 8, 9, 6);
+    const list3 = new LinkedList(5, 4, 4, 7, 8);
+    expect(list1.kthLast(2)).to.eql(3);
+    expect(list2.kthLast(3)).to.eql(6);
+    expect(list3.kthLast(0)).to.eql(8);
   });
 
 });
+
+
+
